703d0e63de92fbbffbb3ac2b253328e757a1c1f8
Stephen Zhou <hi@hyoban.cc>
2024-02-16 00:04:33 +0800

    feat: style option

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 
 import process from 'node:process'
 
+import type { StylisticCustomizeOptions } from '@stylistic/eslint-plugin'
 import stylistic from '@stylistic/eslint-plugin'
 import type { UnifiedFlatConfig } from 'eslint-flat-config'
 import { config } from 'eslint-flat-config'
@@ -19,205 +20,207 @@ const GLOB_TS = '**/*.?([cm])ts'
 const GLOB_TSX = '**/*.?([cm])tsx'
 
 export type Options = {
-  react?: boolean
-  next?: boolean
-  typescript?: {
-    strict?: boolean
-    typeChecked?: boolean
-  }
+	react?: boolean
+	next?: boolean
+	typescript?: {
+		strict?: boolean
+		typeChecked?: boolean
+	}
+	style?: StylisticCustomizeOptions
 }
 
 const reactRelatedPackages = ['react', 'react-dom']
 const nextRelatedPackages = ['next']
 
 export default async function hyoban(options?: Options) {
-  const {
-    react = reactRelatedPackages.some(element => isPackageExists(element)),
-    next = nextRelatedPackages.some(element => isPackageExists(element)),
-    typescript,
-  } = options ?? {}
+	const {
+		react = reactRelatedPackages.some(element => isPackageExists(element)),
+		next = nextRelatedPackages.some(element => isPackageExists(element)),
+		typescript,
+		style,
+	} = options ?? {}
 
-  const requiredPackages = [
-    react && '@eslint-react/eslint-plugin',
-    react && 'eslint-plugin-react-hooks',
-    next && '@next/eslint-plugin-next',
-  ].filter(Boolean)
-  await ensurePackages(requiredPackages)
+	const requiredPackages = [
+		react && '@eslint-react/eslint-plugin',
+		react && 'eslint-plugin-react-hooks',
+		next && '@next/eslint-plugin-next',
+	].filter(Boolean)
+	await ensurePackages(requiredPackages)
 
-  const { strict = true, typeChecked = true } = typescript ?? {}
-  const typescriptPresets = strict
-    ? (typeChecked
-        ? [
-            ...tseslint.configs.strictTypeChecked,
-            ...tseslint.configs.stylisticTypeChecked,
-          ]
-        : [
-            ...tseslint.configs.strict,
-            ...tseslint.configs.stylistic,
-          ])
-    : (typeChecked
-        ? [
-            ...tseslint.configs.recommendedTypeChecked,
-            ...tseslint.configs.stylisticTypeChecked,
-          ]
-        : [
-            ...tseslint.configs.recommended,
-            ...tseslint.configs.stylistic,
-          ])
+	const { strict = true, typeChecked = true } = typescript ?? {}
+	const typescriptPresets = strict
+		? (typeChecked
+				? [
+						...tseslint.configs.strictTypeChecked,
+						...tseslint.configs.stylisticTypeChecked,
+					]
+				: [
+						...tseslint.configs.strict,
+						...tseslint.configs.stylistic,
+					])
+		: (typeChecked
+				? [
+						...tseslint.configs.recommendedTypeChecked,
+						...tseslint.configs.stylisticTypeChecked,
+					]
+				: [
+						...tseslint.configs.recommended,
+						...tseslint.configs.stylistic,
+					])
 
-  return config(
-    {
-      rules: {
-        'prefer-template': 'error',
-        'no-console': ['warn', { allow: ['warn', 'error'] }],
-        'no-restricted-syntax': [
-          'error',
-          {
-            selector: 'TSEnumDeclaration',
-            message: 'We should not use Enum',
-          },
-        ],
-      },
-    },
-    stylistic.configs['recommended-flat'],
-    {
-      plugins: {
-        antfu: eslintPluginAntfu,
-      },
-      rules: {
-        'antfu/consistent-list-newline': 'error',
-        'antfu/if-newline': 'error',
-        'antfu/top-level-function': 'error',
-      },
-    },
-    [
-      eslintPluginUnicorn.configs['flat/recommended'],
-      {
-        rules: {
-          // we should not restrict how we name our variables
-          'unicorn/prevent-abbreviations': 'off',
-          'unicorn/catch-error-name': 'off',
-          // https://github.com/sindresorhus/meta/discussions/7
-          'unicorn/no-null': 'off',
-          // https://github.com/orgs/web-infra-dev/discussions/10
-          'unicorn/prefer-top-level-await': 'off',
-        },
-      },
-    ],
-    {
-      name: 'Import sort',
-      plugins: {
-        'simple-import-sort': simpleImportSort,
-        'import': eslintPluginImport,
-      },
-      rules: {
-        'simple-import-sort/imports': 'error',
-        'simple-import-sort/exports': 'error',
-        'import/first': 'error',
-        'import/newline-after-import': 'error',
-        'import/no-duplicates': 'error',
-        'antfu/import-dedupe': 'error',
+	return config(
+		{
+			rules: {
+				'prefer-template': 'error',
+				'no-console': ['warn', { allow: ['warn', 'error'] }],
+				'no-restricted-syntax': [
+					'error',
+					{
+						selector: 'TSEnumDeclaration',
+						message: 'We should not use Enum',
+					},
+				],
+			},
+		},
+		stylistic.configs.customize({ indent: 'tab', ...style }) as UnifiedFlatConfig,
+		{
+			plugins: {
+				antfu: eslintPluginAntfu,
+			},
+			rules: {
+				'antfu/consistent-list-newline': 'error',
+				'antfu/if-newline': 'error',
+				'antfu/top-level-function': 'error',
+			},
+		},
+		[
+			eslintPluginUnicorn.configs['flat/recommended'],
+			{
+				rules: {
+					// we should not restrict how we name our variables
+					'unicorn/prevent-abbreviations': 'off',
+					'unicorn/catch-error-name': 'off',
+					// https://github.com/sindresorhus/meta/discussions/7
+					'unicorn/no-null': 'off',
+					// https://github.com/orgs/web-infra-dev/discussions/10
+					'unicorn/prefer-top-level-await': 'off',
+				},
+			},
+		],
+		{
+			name: 'Import sort',
+			plugins: {
+				'simple-import-sort': simpleImportSort,
+				'import': eslintPluginImport,
+			},
+			rules: {
+				'simple-import-sort/imports': 'error',
+				'simple-import-sort/exports': 'error',
+				'import/first': 'error',
+				'import/newline-after-import': 'error',
+				'import/no-duplicates': 'error',
+				'antfu/import-dedupe': 'error',
 
-        'antfu/no-import-dist': 'error',
-        'antfu/no-import-node-modules-by-path': 'error',
-      },
-    },
-    [
-      ...typescriptPresets,
-      typeChecked
-        ? {
-            languageOptions: {
-              parserOptions: {
-                project: true,
-                tsconfigRootDir: process.cwd(),
-              },
-            },
-          }
-        : {},
-      {
-        rules: {
-          'no-unused-vars': 'off',
-          '@typescript-eslint/no-unused-vars': [
-            'error',
-            {
-              argsIgnorePattern: '^_',
-              varsIgnorePattern: '^_',
-            },
-          ],
+				'antfu/no-import-dist': 'error',
+				'antfu/no-import-node-modules-by-path': 'error',
+			},
+		},
+		[
+			...typescriptPresets,
+			typeChecked
+				? {
+						languageOptions: {
+							parserOptions: {
+								project: true,
+								tsconfigRootDir: process.cwd(),
+							},
+						},
+					}
+				: {},
+			{
+				rules: {
+					'no-unused-vars': 'off',
+					'@typescript-eslint/no-unused-vars': [
+						'error',
+						{
+							argsIgnorePattern: '^_',
+							varsIgnorePattern: '^_',
+						},
+					],
 
-          '@typescript-eslint/consistent-type-imports': 'error',
-          '@typescript-eslint/no-import-type-side-effects': 'error',
+					'@typescript-eslint/consistent-type-imports': 'error',
+					'@typescript-eslint/no-import-type-side-effects': 'error',
 
-          '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
-          '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-        },
-      },
-      strict
-        ? {
-            rules: {
-              '@typescript-eslint/no-non-null-assertion': 'off',
-            },
-          }
-        : {},
-      typeChecked
-        ? {
-            rules: {
-              '@typescript-eslint/consistent-type-exports': 'error',
-              '@typescript-eslint/no-misused-promises': [
-                'error',
-                {
-                  checksVoidReturn: {
-                    arguments: false,
-                    attributes: false,
-                  },
-                },
-              ],
-            },
-          }
-        : {},
-    ],
-    async () => {
-      if (!react)
-        return
-      const eslintReact = await interopDefault(import('@eslint-react/eslint-plugin'))
-      return [
-        eslintReact.configs.all,
-        {
-          files: [GLOB_TS, GLOB_TSX],
-          rules: {
-            '@eslint-react/naming-convention/filename': 'off',
-          },
-        },
-        typeChecked
-          ? {
-              rules: {
-                '@eslint-react/no-leaked-conditional-rendering': 'error',
-              },
-            }
-          : {},
-      ] as UnifiedFlatConfig[]
-    },
-    async () => {
-      if (!react)
-        return
-      const reactHooks = await interopDefault(import('eslint-plugin-react-hooks'))
-      return {
-        plugins: {
-          'react-hooks': reactHooks,
-        },
-        rules: reactHooks.configs.recommended.rules,
-      } as UnifiedFlatConfig
-    },
-    async () => {
-      if (!next)
-        return
-      const eslintPluginNext = await interopDefault(import('@next/eslint-plugin-next'))
-      return {
-        plugins: {
-          '@next/next': eslintPluginNext,
-        },
-        rules: eslintPluginNext.configs.recommended.rules,
-      } as UnifiedFlatConfig
-    },
-  )
+					'@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+					'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+				},
+			},
+			strict
+				? {
+						rules: {
+							'@typescript-eslint/no-non-null-assertion': 'off',
+						},
+					}
+				: {},
+			typeChecked
+				? {
+						rules: {
+							'@typescript-eslint/consistent-type-exports': 'error',
+							'@typescript-eslint/no-misused-promises': [
+								'error',
+								{
+									checksVoidReturn: {
+										arguments: false,
+										attributes: false,
+									},
+								},
+							],
+						},
+					}
+				: {},
+		],
+		async () => {
+			if (!react)
+				return
+			const eslintReact = await interopDefault(import('@eslint-react/eslint-plugin'))
+			return [
+				eslintReact.configs.all,
+				{
+					files: [GLOB_TS, GLOB_TSX],
+					rules: {
+						'@eslint-react/naming-convention/filename': 'off',
+					},
+				},
+				typeChecked
+					? {
+							rules: {
+								'@eslint-react/no-leaked-conditional-rendering': 'error',
+							},
+						}
+					: {},
+			] as UnifiedFlatConfig[]
+		},
+		async () => {
+			if (!react)
+				return
+			const reactHooks = await interopDefault(import('eslint-plugin-react-hooks'))
+			return {
+				plugins: {
+					'react-hooks': reactHooks,
+				},
+				rules: reactHooks.configs.recommended.rules,
+			} as UnifiedFlatConfig
+		},
+		async () => {
+			if (!next)
+				return
+			const eslintPluginNext = await interopDefault(import('@next/eslint-plugin-next'))
+			return {
+				plugins: {
+					'@next/next': eslintPluginNext,
+				},
+				rules: eslintPluginNext.configs.recommended.rules,
+			} as UnifiedFlatConfig
+		},
+	)
 }
--- src/utils.ts
+++ src/utils.ts
@@ -5,28 +5,28 @@ import { isPackageExists } from 'local-pkg'
 export type Awaitable<T> = T | Promise<T>
 
 export async function interopDefault<T>(m: Awaitable<T>): Promise<T extends { default: infer U } ? U : T> {
-  const resolved = await m
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-  return (resolved as any).default || resolved
+	const resolved = await m
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+	return (resolved as any).default || resolved
 }
 
 export async function ensurePackages(packages: string[]) {
-  if (process.env.CI ?? !process.stdout.isTTY)
-    return
+	if (process.env.CI ?? !process.stdout.isTTY)
+		return
 
-  const nonExistingPackages = packages.filter(i => !isPackageExists(i))
-  if (nonExistingPackages.length === 0)
-    return
+	const nonExistingPackages = packages.filter(i => !isPackageExists(i))
+	if (nonExistingPackages.length === 0)
+		return
 
-  const { default: prompts } = await import('prompts')
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { result } = await prompts([
-    {
-      message: `${nonExistingPackages.length === 1 ? 'Package is' : 'Packages are'} required for this config: ${nonExistingPackages.join(', ')}. Do you want to install them?`,
-      name: 'result',
-      type: 'confirm',
-    },
-  ])
-  if (result)
-    await import('@antfu/install-pkg').then(i => i.installPackage(nonExistingPackages, { dev: true }))
+	const { default: prompts } = await import('prompts')
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const { result } = await prompts([
+		{
+			message: `${nonExistingPackages.length === 1 ? 'Package is' : 'Packages are'} required for this config: ${nonExistingPackages.join(', ')}. Do you want to install them?`,
+			name: 'result',
+			type: 'confirm',
+		},
+	])
+	if (result)
+		await import('@antfu/install-pkg').then(i => i.installPackage(nonExistingPackages, { dev: true }))
 }
