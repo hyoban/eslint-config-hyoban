8b4df13b7a723ba7029f387f95c835a1f39817a4
Stephen Zhou <hi@hyoban.cc>
2024-02-21 20:30:38 +0800

    feat: cli to update config

---
+++ src/bin/index.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process'
+import fs from 'node:fs'
+
+import { ensurePackages } from '../utils'
+// @ts-expect-error no types
+import settings from './settings.txt'
+
+try {
+  // check if git status is clean
+  execSync('git diff-index --quiet HEAD --')
+  ensurePackages([
+    'lint-staged',
+    'simple-git-hooks',
+  ]).then(() => {
+    const packageJson = fs.readFileSync('package.json', 'utf8')
+    const packageJsonParsed = JSON.parse(packageJson) as Record<string, Record<string, unknown>>
+
+    packageJsonParsed['lint-staged'] = { '*': 'eslint --fix' }
+    packageJsonParsed['simple-git-hooks'] = { 'pre-commit': 'lint-staged' }
+    packageJsonParsed.scripts = {
+      ...packageJsonParsed.scripts,
+      prepare: 'simple-git-hooks',
+    }
+    fs.writeFileSync('package.json', JSON.stringify(packageJsonParsed, null, 2))
+
+    fs.writeFileSync('./vscode/settings.json', settings as string)
+  }).catch(() => {
+    console.error('Failed to install required packages')
+  })
+}
+catch {
+  console.error('Please commit all changes before running this script')
+  process.exit(1)
+}
