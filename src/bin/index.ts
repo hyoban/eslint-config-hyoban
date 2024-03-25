95cb6012eb3423d72ff47b2ab7a2646585f579a0
Stephen Zhou <hi@hyoban.cc>
2024-03-25 12:46:39 +0800

    fix(cli): do not require git hooks

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -6,23 +6,22 @@ import { ensurePackages } from '../utils'
 // @ts-expect-error - bunchee will handle this
 import settings from './settings.txt'
 
+interface PackageJson {
+  scripts: Record<string, string>,
+  type?: string,
+}
+
 async function main() {
   try {
     await ensurePackages([
       'eslint',
       'eslint-config-hyoban',
-      'lint-staged',
-      'simple-git-hooks',
     ])
 
     const packageJson = fs.readFileSync('package.json', 'utf8')
-    const packageJsonParsed = JSON.parse(packageJson) as Record<string, Record<string, unknown>>
-
-    packageJsonParsed['lint-staged'] = { '*': 'eslint --fix' }
-    packageJsonParsed['simple-git-hooks'] = { 'pre-commit': 'pnpm lint-staged' }
+    const packageJsonParsed = JSON.parse(packageJson) as PackageJson
     packageJsonParsed.scripts = {
       ...packageJsonParsed.scripts,
-      prepare: 'simple-git-hooks',
       lint: 'eslint .',
     }
     fs.writeFileSync('package.json', `${JSON.stringify(packageJsonParsed, null, 2)}\n`)
@@ -33,7 +32,12 @@ async function main() {
     fs.writeFileSync('.vscode/settings.json', settings as string)
 
     const eslintConfig = 'import hyoban from \'eslint-config-hyoban\'\n\nexport default hyoban()\n'
-    fs.writeFileSync('eslint.config.js', eslintConfig)
+    fs.writeFileSync(
+      packageJsonParsed.type === 'module'
+        ? 'eslint.config.js'
+        : 'eslint.config.mjs',
+      eslintConfig,
+    )
   }
   catch (err) {
     if (err instanceof Error)
