43c89881682309073a477c0dd975ee774e3b0f87
Stephen Zhou <hi@hyoban.cc>
2024-05-21 12:08:39 +0800

    chore: manually typegen

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,10 @@
 // @ts-check
-import typegen from 'eslint-typegen'
 
 // eslint-disable-next-line antfu/no-import-dist
 import hyoban from './dist/index.js'
 
-export default typegen(
-  hyoban({
-    react: 'vite',
-    strict: true,
-    typeChecked: true,
-  }),
-)
+export default hyoban({
+  react: 'vite',
+  strict: true,
+  typeChecked: true,
+})
---
+++ scripts/typegen.ts
@@ -0,0 +1,22 @@
+import fs from 'node:fs/promises'
+
+import { builtinRules } from 'eslint/use-at-your-own-risk'
+import { flatConfigsToPlugins, pluginsToRulesDTS } from 'eslint-typegen/core'
+
+import hyoban from '../src'
+
+const plugins = await flatConfigsToPlugins(
+  [
+    {
+      plugins: {
+        '': {
+          rules: Object.fromEntries(builtinRules.entries()),
+        },
+      },
+    },
+    ...(await hyoban({ react: 'vite', strict: true, typeChecked: true })),
+  ],
+)
+const dts = await pluginsToRulesDTS(plugins)
+
+await fs.writeFile('eslint-typegen.d.ts', dts)
