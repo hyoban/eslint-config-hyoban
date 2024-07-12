9856b0d05d21d76ce7776355bdaa44a47585a5fe
Stephen Zhou <hi@hyoban.cc>
2024-07-12 20:27:38 +0800

    fix: rules

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -59,7 +59,7 @@ export function typeScriptConfigs({
           }
         : {
             rules: {
-              '@typescript-eslint/ban-ts-comment': 'warn',
+              '@typescript-eslint/ban-ts-comment': 'off',
               '@typescript-eslint/no-explicit-any': 'off',
               '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
             },
