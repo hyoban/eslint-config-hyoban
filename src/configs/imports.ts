955247273da0179ffaab6f11fdd58e1ead818d78
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-05 16:10:29 +0800

    refactor!: migrate to eslint-plugin-import-lite

--- src/configs/imports.ts
+++ src/configs/imports.ts
@@ -1,6 +1,6 @@
 import type { ESLint } from 'eslint'
 import pluginAntfu from 'eslint-plugin-antfu'
-import * as pluginImport from 'eslint-plugin-import-x'
+import * as pluginImport from 'eslint-plugin-import-lite'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 import type { LinterConfig } from '../utils'
@@ -10,22 +10,22 @@ export function importConfig(): LinterConfig {
     name: 'extend/import',
     plugins: {
       'simple-import-sort': simpleImportSort,
-      'import-x': pluginImport as unknown as ESLint.Plugin,
+      'import-lite': pluginImport as unknown as ESLint.Plugin,
       'antfu': pluginAntfu,
     },
     rules: {
       'sort-imports': 'off',
-      'import-x/order': 'off',
+      'import-lite/order': 'off',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
 
-      'import-x/first': 'error',
-      'import-x/newline-after-import': 'error',
+      'import-lite/first': 'error',
+      'import-lite/newline-after-import': 'error',
       'no-duplicate-imports': 'off',
-      'import-x/no-duplicates': 'error',
+      'import-lite/no-duplicates': 'error',
       'antfu/import-dedupe': 'error',
 
-      'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      'import-lite/consistent-type-specifier-style': ['error', 'prefer-top-level'],
 
       'antfu/no-import-dist': 'error',
       'antfu/no-import-node-modules-by-path': 'error',
