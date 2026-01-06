4ef1af7805073cabdde5d9561b426a8dd804d784
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-06 22:15:20 +0800

    chore: update type

--- eslint-typegen.d.ts
+++ eslint-typegen.d.ts
@@ -1961,6 +1961,51 @@ export interface RuleOptions {
    */
   'implicit-arrow-linebreak'?: Linter.RuleEntry<ImplicitArrowLinebreak>
   /**
+   * Enforce or ban the use of inline type-only markers for named imports.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/consistent-type-specifier-style/README.md
+   */
+  'import-lite/consistent-type-specifier-style'?: Linter.RuleEntry<ImportLiteConsistentTypeSpecifierStyle>
+  /**
+   * Ensure all exports appear after other statements.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/exports-last/README.md
+   */
+  'import-lite/exports-last'?: Linter.RuleEntry<[]>
+  /**
+   * Ensure all imports appear before other statements.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/first/README.md
+   */
+  'import-lite/first'?: Linter.RuleEntry<ImportLiteFirst>
+  /**
+   * Enforce a newline after import statements.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/newline-after-import/README.md
+   */
+  'import-lite/newline-after-import'?: Linter.RuleEntry<ImportLiteNewlineAfterImport>
+  /**
+   * Forbid default exports.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/no-default-export/README.md
+   */
+  'import-lite/no-default-export'?: Linter.RuleEntry<[]>
+  /**
+   * Forbid repeated import of the same module in multiple places.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/no-duplicates/README.md
+   */
+  'import-lite/no-duplicates'?: Linter.RuleEntry<ImportLiteNoDuplicates>
+  /**
+   * Forbid the use of mutable exports with `var` or `let`.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/no-mutable-exports/README.md
+   */
+  'import-lite/no-mutable-exports'?: Linter.RuleEntry<[]>
+  /**
+   * Forbid named default exports.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/no-named-default/README.md
+   */
+  'import-lite/no-named-default'?: Linter.RuleEntry<[]>
+  /**
+   * Prefer a default export if module exports a single name or multiple names.
+   * @see https://github.com/9romise/eslint-plugin-import-lite/blob/main/src/rules/prefer-default-export/README.md
+   */
+  'import-lite/prefer-default-export'?: Linter.RuleEntry<ImportLitePreferDefaultExport>
+  /**
    * Enforce consistent indentation
    * @see https://eslint.org/docs/latest/rules/indent
    * @deprecated
@@ -7832,6 +7877,24 @@ type IdMatch = []|[string]|[string, {
 }]
 // ----- implicit-arrow-linebreak -----
 type ImplicitArrowLinebreak = []|[("beside" | "below")]
+// ----- import-lite/consistent-type-specifier-style -----
+type ImportLiteConsistentTypeSpecifierStyle = []|[("top-level" | "inline" | "prefer-top-level")]
+// ----- import-lite/first -----
+type ImportLiteFirst = []|[("absolute-first" | "disable-absolute-first")]
+// ----- import-lite/newline-after-import -----
+type ImportLiteNewlineAfterImport = []|[{
+  count?: number
+  exactCount?: boolean
+  considerComments?: boolean
+}]
+// ----- import-lite/no-duplicates -----
+type ImportLiteNoDuplicates = []|[{
+  "prefer-inline"?: boolean
+}]
+// ----- import-lite/prefer-default-export -----
+type ImportLitePreferDefaultExport = []|[{
+  target?: ("single" | "any")
+}]
 // ----- indent -----
 type Indent = []|[("tab" | number)]|[("tab" | number), {
   SwitchCase?: number
